4bbd59f174b6d5f8afcdc9c0c99c9379398f4c06
LeonDEvans <evansleon87@gmail.com>
2024-06-30 06:56:50 -0400

    update UI

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+let firstNumber = 0;
+let operator = 0;
+let lastNumber = 0;
+
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+
+function operate(a, o , b){
+    if(o = '+'){
+        add()
+    }else if (o = '-') {
+       subtract()
+    }else if (o = '*') {
+        multiply()
+    }else if (o = "/") {
+        divide()
+    }else {
+        console.log('error')
+    }
+
+
+}
+
+
+
 
