5941fea9c23e598b60eb99f998cad225bead7bd1
LeonDEvans <evansleon87@gmail.com>
2024-07-02 21:44:51 -0400

    Functional buttons

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
 let firstNumber = 0;
 let operator = 0;
 let lastNumber = 0;
+
+let screen = document.querySelector('.screen')
+
+
+
+
+
+
 
 function add(){
 
@@ -31,6 +40,63 @@ function operate(a, o , b){
 
 }
 
+function display(){
+    
+}
 
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener("click", function(event) {
+    const target = event.target;
+    switch (target.id) {
+        case "1":
+        console.log('hello');
+            break;
+        case "2":
+            console.log('hello');
+            break;
+        case '3':
+            console.log('hello');
+            break;
+        case '4':
+            console.log('hello');
+            break;
+        case '5':
+            console.log('hello');
+            break;
+        case '6':
+            console.log('hello');
+            break;
+        case '7':
+            console.log('hello');
+            break;
+        case '8':
 
+            break;
+        case '9':
 
+            break;
+        case '0':
+
+            break;
+        case '+':
+
+            break;
+        case '-':
+
+            break;
+        case 'x':
+
+            break;
+        case '/':
+
+            break;
+        case 'clear':
+            break;
+        
+        default: 
+        console.log('happy')
+
+    };
+  });
+})
+})
