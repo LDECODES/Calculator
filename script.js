570a84e5e2cd47593e6f1dd473bb81eca7abba40
evansleon87 <evansleon87@gmail.com>
2024-08-24 23:31:24 +0000

    fix saveEquation function

--- script.js
+++ script.js
@@ -1,17 +1,173 @@
-document.addEventListener("DOMContentLoaded", function() {   
-    function add(){
+document.addEventListener("DOMContentLoaded", function() {
 
+    let value = 0;
+    let pScreen = document.querySelector('.primary-screen')
+    let sScreen = document.querySelector('.secondary-screen')
+    let savedEquation = ''
+    let savedScreen
+    let equation = {
+        firstNumber: [],
+        secondNumber: [],
+        firstOperator: '',
+        secondOperator: '',
+    }
+       
+
+    document.querySelectorAll('button').forEach(button => {
+        button.addEventListener("click", function(event) {
+        const target = event.target;
+        switch (target.id) {
+            case '1':
+                savedEquation = saveEquation(target.id)
+                display(savedEquation)
+                break
+            case '2':
+                value = target.id
+                display()
+                break
+            case '3':
+                value = target.id
+                display(value)
+                break
+            case '4':
+                value = target.id
+                display(value)
+                break
+            case '5':
+                value = target.id
+                display(value)
+                break
+            case '6':
+                value = target.id
+                display(value)
+                break
+            case '7':
+                value = target.id
+                display(value)
+                break
+            case '8':
+                value = target.id
+                display(value)
+                break
+            case '9':
+                value = target.id
+                display(value)
+                break
+            case '0':
+                value = target.id
+                display(value)
+                break
+            case '+':
+                value = target.id
+                display(value)
+                break
+            case '-':
+                value = target.id
+                display(value)
+                break
+            case '/':
+                value = target.id
+                display(value)
+                break
+            case 'x':
+                value = target.id
+                display(value)
+                break
+            case '=':
+                operate()
+                break
+            case 'clear':
+                clear()
+                break
+            case 'Del':
+                del()
+                break
+            case '.':
+                value = target.id
+                display(value)
+                break
+            default:
+                break
+        }
+    })
+
+
+    function add(a, b){
+        return a + b
+  
+    }
+    function subtract(a, b){
+        return a - b
     }
 
-    function subtract(){
+    function divide(a, b){ 
+        return a / b
+    } 
 
+    function multiply(a, b){ 
+        returna * b
+    } 
+
+    function saveEquation (value) {
+        if(equation.firstNumber.length > 0 ){
+            if (value == "+" ||value ==  "-" || value == "x" || value == "/" ){
+                equation.firstOperator.push(value)
+            }else {
+                equation.firstNumber.push(value)
+            }
+        }else if(!equation.firstOperator == '' ) {
+            equation.secondNumber.push(value)
+        }else if (!equation.secondNumber.length < 1 || equation.secondNumber == undefined && value == "+" ||value ==  "-" || value == "x" || value == "/" ){
+            equation.secondOperator.push(value)
+        }
+
+        let firstNumber = equation.firstNumber.join('')
+        let secondNumber = equation.secondNumber.join('')
+        let firstOperator = equation.firstOperator
+        let secondOperator = equation.secondOperator
+
+
+        return `${firstNumber} ${firstOperator} ${secondNumber} ${secondOperator}`
     }
 
-    function divide(){
+
+   
+
+    function operate(operator, a, b){
+        switch (operator) {
+            case '+':
+                add(a, b)
+                break
+
+            case '-':
+                subtract(a, b)
+                break
+
+            case '/':
+                divide(a, b)
+                break
+
+            case 'x':
+                multiply(a, b)
+                break
+            default:
+              return('something went wrong with the operator');
+        }
+     function del (idk) {
 
     }
-
-    function multiply(){
-        
+    
+    function display(value){
+        primaryScreen.textContent = value
     }
+
+    function clear(){
+    primaryScreen.textContent = ''
+    }
+    
+}
+    })
+
 })
+
+
